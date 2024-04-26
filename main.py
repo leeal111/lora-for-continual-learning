ebd545cb461f1ccd803b1d59c135553699b23d83
leeal <leeal111@163.com>
2024-04-26 14:11:19 +0800

    implement args init

---
+++ main.py
@@ -0,0 +1,41 @@
+import argparse
+import logging
+from train import set_random_seed
+from utils import init_args
+
+# [120, 48, 125, 24, 6]
+# ["gaugan", "biggan", "wild", "whichfaceisreal", "san"]
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--batch_size", type=int, default=32)
+parser.add_argument("--workers_num", type=int, default=8)
+parser.add_argument("--gpus", type=int, nargs="+", default=[1])
+parser.add_argument("--seed", type=int, default=10)
+parser.add_argument("--lr", type=float, default=5e-4)
+parser.add_argument("--epochs", type=int, default=10)
+parser.add_argument("--rank", type=int, default=10)
+
+parser.add_argument("--n_clusters", type=int, default=5)
+parser.add_argument("--dataset_path", type=str, default="./datas/CDDB")
+parser.add_argument("--dataset_name", type=str)
+parser.add_argument("--pretrain_model_path", type=str, default="./pths/B_16.pth")
+parser.add_argument("--pretrain_model_name", type=str)
+parser.add_argument("--classes_num", type=int, default=10)
+parser.add_argument("--tasks_num", type=int, default=5)
+parser.add_argument("--result_path", type=str, default="./results")
+
+parser.add_argument("--class_num_per_task_list", nargs="+")
+parser.add_argument("--tasks_name", nargs="+")
+parser.add_argument("--tasks_lr_T", nargs="+")
+
+
+parser.add_argument("--train_type", type=str, default="lora", choices=["lora"])
+
+cfg = parser.parse_args()
+init_args(cfg)
+
+logging.info(f"====>")
+logging.info(f"experiment settings:")
+for arg_name, arg_value in cfg.__dict__.items():
+    logging.info(f"{arg_name} : {str(arg_value)}")
+logging.info(f"<====")
---
+++ utils.py
@@ -0,0 +1,60 @@
+import logging
+from os import makedirs
+from os.path import join,basename,splitext
+import time
+
+def init_args(args):
+
+    # input verify
+    if args.dataset_name is None:
+        args.dataset_name = splitext(basename(args.dataset_path))[0]
+    if args.pretrain_model_name is None:
+        args.pretrain_model_name = splitext(basename(args.pretrain_model_path))[0]
+    assert args.classes_num >= args.tasks_num
+    if args.tasks_name is not None:
+        assert len(args.tasks_name)==args.tasks_num
+    if args.tasks_lr_T is not None:
+        assert len(args.tasks_lr_T)==args.tasks_num
+    if args.class_num_per_task_list is not None:
+        assert len(args.class_num_per_task_list)==args.tasks_num
+        assert sum(args.class_num_per_task_list)==args.classes_num
+    else:
+        class_num_per_task = args.classes_num // args.tasks_num
+        if class_num_per_task*args.tasks_num!=args.classes_num:
+            class_num_per_task+=1
+        args.class_num_per_task_list = []
+        while sum(args.class_num_per_task_list) + class_num_per_task < args.classes_num:
+            args.class_num_per_task_list.append(class_num_per_task)
+        offset = args.classes_num - sum(args.class_num_per_task_list)
+        args.class_num_per_task_list.append(offset)
+
+    # make output diretory
+    args.log_path=join(args.result_path,f"log")
+    args.weight_path=join(args.result_path,f"weight")
+    args.center_path=join(args.result_path,f"center")
+    makedirs(args.log_path, exist_ok=True)
+    makedirs(args.weight_path, exist_ok=True)
+    makedirs(args.weight_path, exist_ok=True)
+
+    # other
+    init_logging(args.log_path)
+    
+
+def init_logging(result_path):
+    logger = logging.getLogger("")
+    if not logger.handlers:
+        logging.basicConfig(
+            level=logging.INFO,
+            format="[%(asctime)s-%(levelname)s] %(message)s",
+            datefmt="%y-%m-%d %H:%M:%S",
+            filename=join(result_path, f"{time.strftime("%Y%m%d_%H%M%S")}.log"),
+            filemode="w",
+        )
+        console = logging.StreamHandler()
+        formatter = logging.Formatter("[%(asctime)s-%(levelname)s] %(message)s")
+        console.setFormatter(formatter)
+        logger.addHandler(console)
+    
+
+
+
